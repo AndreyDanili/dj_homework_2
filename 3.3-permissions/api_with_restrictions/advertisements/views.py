d0f7dbfe2ee6ce7667777d9e82d0207f66c02a3d
Andrey Danilishin <adanilishin@gmail.com>
2022-06-12 10:07:47 +0300

    Add filter status and permit delete

--- 3.3-permissions/api_with_restrictions/advertisements/views.py
+++ 3.3-permissions/api_with_restrictions/advertisements/views.py
@@ -11,7 +11,7 @@ from advertisements.serializers import AdvertisementSerializer, FavoritesSeriali
 
 class AdvertisementViewSet(ModelViewSet):
     """ViewSet для объявлений."""
-    
+
     queryset = Advertisement.objects.all()
     serializer_class = AdvertisementSerializer
     filterset_class = AdvertisementFilter
@@ -19,7 +19,7 @@ class AdvertisementViewSet(ModelViewSet):
     def get_permissions(self):
         """Получение прав для действий."""
 
-        if self.action in ["create", "update", "partial_update", ]:
+        if self.action in ["create", "update", "partial_update", "destroy"]:
             return [IsAuthenticated(), IsOwnerOrReadOnly()]
         return []
 
